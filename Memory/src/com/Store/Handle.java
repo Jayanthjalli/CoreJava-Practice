370f8fde62adda71695f0f2e34b1b96839a6b25e
Jayanthjalli <jalllijayanthjalli@gmail.com>
2026-05-21 10:59:55 +0530

    sa

---
+++ Memory/src/com/Store/Handle.java
@@ -0,0 +1,12 @@
+package com.Store;
+
+public class Handle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("it is handling");
+		
+
+	}
+
+}
